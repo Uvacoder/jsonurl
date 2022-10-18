64418dcf32c7cb01f238f5ce4c1c8aa08329a06d
kopyl <oleggggg1997@gmail.com>
2022-10-18 06:11:07 +0300

    Remove unused code and comments

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,8 @@
 import type { NextPage } from "next";
 import styles from "../styles/Home.module.css";
-import { useState, useEffect } from "react";
 
 const Home: NextPage = () => {
-    const [data, setData] = useState<any>(null);
-
-    useEffect(() => {
-        fetch(`https://jsonurl.vercel.app/{"name": "Oleh"}`)
-            .then((res) => res.json())
-            .then((data) => {
-                setData(data);
-                console.log(data);
-            });
-    }, []);
-
-    return (
-        <div className={styles.container}>
-            <p>{data?.name}</p>
-        </div>
-    );
+    return <div className={styles.container}></div>;
 };
 
 export default Home;
