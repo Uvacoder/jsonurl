2ef3e8aecde53801cd73c88b88e644476b93cff4
kopyl <oleggggg1997@gmail.com>
2022-10-18 14:44:51 +0300

    Trying to connect to DB on app's launch

--- src/api/mongo.ts
+++ src/api/mongo.ts
@@ -4,27 +4,19 @@ const username = process.env.MONGO_USERNAME;
 const password = process.env.MONGO_PASSWORD;
 const uri = `mongodb+srv://${username}:${password}@jsonurl.bdigtqz.mongodb.net/?retryWrites=true&w=majority`;
 
-export const insertRecord = async (collection: string, data: any) => {
-    const client = await new MongoClient(uri, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        serverApi: ServerApiVersion.v1,
-    });
+const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverApi: ServerApiVersion.v1,
+});
 
+export const insertRecord = async (collection: string, data: any) => {
     const db = client.db('jsonurl');
     await db.collection(collection).insertOne(data);
-    client.close();
 };
 
 export const getRecord = async (id: string) => {
-    const client = await new MongoClient(uri, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        serverApi: ServerApiVersion.v1,
-    });
-
     const db = client.db('jsonurl');
     const record = await db.collection('urls').findOne({ _id: id });
-    client.close();
     return record;
 };
