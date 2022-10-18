54091d8e01abca6b2949fbd750ef4ce93ee14874
kopyl <oleggggg1997@gmail.com>
2022-10-18 06:26:02 +0300

    Make more real mongo insert method

--- src/api/mongo.ts
+++ src/api/mongo.ts
@@ -4,19 +4,14 @@ const username = process.env.MONGO_USERNAME;
 const password = process.env.MONGO_PASSWORD;
 const uri = `mongodb+srv://${username}:${password}@jsonurl.bdigtqz.mongodb.net/?retryWrites=true&w=majority`;
 
-export const insertRecord = async () => {
+export const insertRecord = async (collection: string, data: any) => {
     const client = await new MongoClient(uri, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
         serverApi: ServerApiVersion.v1,
     });
 
-    const db = client.db('test');
-
-    await db.collection('users').insertOne({
-        name: 'user',
-        age: 24,
-    });
-
+    const db = client.db('jsonurl');
+    await db.collection(collection).insertOne(data);
     client.close();
 };
