470b44930ed397c1c0e10b002f8beb5cf92c8bed
kopyl <oleggggg1997@gmail.com>
2022-10-18 03:48:52 +0300

    remove cors

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,9 +2,26 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
+import { useState, useEffect } from "react";
 
 const Home: NextPage = () => {
-    return <div className={styles.container}>HOME</div>;
+    const [data, setData] = useState<any>(null);
+
+    // get data from jsonurl.vercel.app/{"name": "Oleh"}
+    useEffect(() => {
+        fetch(`https://jsonurl.vercel.app/{"name": "Oleh"}`)
+            .then((res) => res.json())
+            .then((data) => {
+                setData(data);
+                console.log(data);
+            });
+    }, []);
+
+    return (
+        <div className={styles.container}>
+            <p>{"s"}</p>
+        </div>
+    );
 };
 
 export default Home;
